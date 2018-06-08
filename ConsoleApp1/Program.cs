1043250b3e7ab4bb40b11c99c1685518c7c31fbd
John Karaplis <jzkarap@gmail.com>
2018-06-08 16:17:24 -0400

    second commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace FiboList
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			Console.Write("GIMME NUMBER: ");
+			string input = Console.ReadLine();
+			while (!Regex.IsMatch(input, @"^\d+$"))
+			{	
+				Console.WriteLine("ME SAY GIMME NUMBER: ");
+				input = Console.ReadLine();
+			}
+
+			decimal userNum = Convert.ToDecimal(input);
+			decimal a = 0;
+			decimal b = 1;
+			decimal total = 0;
+			List<decimal> fiboNums = new List<decimal>();
+
+			fiboNums.Add(total);
+
+			decimal diffBetween = fiboNums[0];
+			decimal sumOfAll = fiboNums[0];
+
+			Console.WriteLine();
+
+			for (int i = 0; fiboNums[i] <= userNum; i++)
+			{
+					total = a + b;
+					a = b;
+					b = total;
+					total = a;
+					fiboNums.Add(total);
+					Console.Write(fiboNums[i] + "  ");
+			}
+
+			Console.WriteLine();
+
+			if (fiboNums[fiboNums.Count - 1] != 1)
+			{
+				diffBetween = (fiboNums[fiboNums.Count - 2] - fiboNums[fiboNums.Count - 3]);
+				Console.WriteLine("\nME THINK DIFFERENCE BETWEEN LAST TWO NUMBERS " + diffBetween + "\n");
+
+				for (int i = 1; i < fiboNums.Count -1; i++)
+				{
+					sumOfAll += fiboNums[i];
+				}
+			}
+			else
+			{
+				Console.WriteLine("\nDREAM BIGGER.");
+			}
+
+			Console.WriteLine(Convert.ToString(fiboNums.Count - 1) +
+							   " FIBONACCIS IN THIS LIST\n");
+
+			Console.WriteLine("THEY ALL ADD TO " + sumOfAll +"\n");
+        }
+    }
+}
---
+++ FibonacciApp/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FibonacciApp
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+		private void btncom_Click(object sender, RoutedEventArgs e)
+			// Runs fibo sequence to a place input by user
+		{
+			decimal a = 0, b = 1, total = 0, i;
+			int n = Convert.ToInt32(txtnum.Text);
+			for (i = 1; i < n; i++)
+			{
+				total = a + b;
+				a = b;
+				b = total;
+				total = a;
+			}
+			txtres.Text = total.ToString();
+		}
+		private void btnclr_Click(object sender, RoutedEventArgs e)
+		{
+			txtnum.Text = String.Empty;
+			txtres.Text = String.Empty;
+		}
+	}
+}
